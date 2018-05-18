6458cfe5110c8d895a254d57e1de09e5d65294bc
Adrian <adrian.silva@quobis.es>
2018-05-18 10:44:37 +0200

    Notification cell label size

--- Sources/Views/Cells/NotificationCell.swift
+++ Sources/Views/Cells/NotificationCell.swift
@@ -19,7 +19,7 @@ open class NotificationCell: UICollectionViewCell {
     open func setupConstraints() {
         self.addSubview(notificationLabel)
         notificationLabel.centerInSuperview()
-        notificationLabel.constraint(equalTo: CGSize(width: 200, height: 22))
+        notificationLabel.constraint(equalTo: CGSize(width: 300, height: 22))
     }
     
     open func configure(with message: MessageType, at indexPath: IndexPath, and messagesCollectionView: MessagesCollectionView) {
